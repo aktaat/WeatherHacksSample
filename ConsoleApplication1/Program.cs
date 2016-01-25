64f240ec569516cfbf9bd73a08d733b0f2c507d6
taat_yuki <sayu_01@me.com>
2016-01-26 06:09:14 +0900

    WeatherHacksを利用したアプリケーションテスト

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
+
+public class Rootobject
+{
+    public DateTime publicTime { get; set; }
+    public string title { get; set; }
+    public Description description { get; set; }
+    public string link { get; set; }
+    public Forecast[] forecasts { get; set; }
+    public Location location { get; set; }
+    public Pinpointlocation[] pinpointLocations { get; set; }
+    public Copyright copyright { get; set; }
+}
+
+public class Description
+{
+    public string text { get; set; }
+    public DateTime publicTime { get; set; }
+}
+
+public class Location
+{
+    public string city { get; set; }
+    public string area { get; set; }
+    public string prefecture { get; set; }
+}
+
+public class Copyright
+{
+    public Provider[] provider { get; set; }
+    public string link { get; set; }
+    public string title { get; set; }
+    public Image image { get; set; }
+}
+
+public class Image
+{
+    public int width { get; set; }
+    public string link { get; set; }
+    public string url { get; set; }
+    public string title { get; set; }
+    public int height { get; set; }
+}
+
+public class Provider
+{
+    public string link { get; set; }
+    public string name { get; set; }
+}
+
+public class Forecast
+{
+    public string dateLabel { get; set; }
+    public string telop { get; set; }
+    public string date { get; set; }
+    public Temperature temperature { get; set; }
+    public Image1 image { get; set; }
+}
+
+public class Temperature
+{
+    public Min min { get; set; }
+    public Max max { get; set; }
+}
+
+public class Min
+{
+    public string celsius { get; set; }
+    public string fahrenheit { get; set; }
+}
+
+public class Max
+{
+    public string celsius { get; set; }
+    public string fahrenheit { get; set; }
+}
+
+public class Image1
+{
+    public int width { get; set; }
+    public string url { get; set; }
+    public string title { get; set; }
+    public int height { get; set; }
+}
+
+public class Pinpointlocation
+{
+    public string link { get; set; }
+    public string name { get; set; }
+}
